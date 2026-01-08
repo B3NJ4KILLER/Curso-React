1dc1968589ed296f414551a6c59c6ea2bac17127
B3NJ4KILLER <benjacevedo12311@gmail.com>
2026-01-08 20:42:05 -0300

    Imprimir Listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import Card, {CardBody} from './components/Card';
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
-  return <Card><CardBody title='Hola Mundo' text='Este es el texto' /></Card>;
+  const list = ["Mili", "Lola", "Benja"];
+  return (
+    <Card>
+      <CardBody title="Hola Mundo" text="Este es el texto" />
+      <List data={list} />
+    </Card>
+  );
 }
 
 export default App;
