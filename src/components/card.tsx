b13a8de40931e024a842bd4e27e4d88dc7dfb743
B3NJ4KILLER <benjacevedo12311@gmail.com>
2026-01-08 20:00:56 -0300

    Children

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,16 +1,18 @@
 // import { Fragment } from "react";
 
+import { ReactNode } from "react";
+
 interface Props {
-    body: string;
+    children: ReactNode;
 }
 
 function Card(props: Props) {
-    const { body } = props;
+    const { children } = props;
     return ( 
         <div className="card" style= {{
         width: "350px",
     }}>
-            <div className="card-body">{body}</div>
+            <div className="card-body">{children}</div>
         </div>
     );
 }
