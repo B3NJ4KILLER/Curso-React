5be86d96ae2a041429b46edcf9dae262e5f98344
B3NJ4KILLER <benjacevedo12311@gmail.com>
2026-01-13 20:32:52 -0300

    Funciones como props

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import List from "./components/List";
 
 function App() {
   const list = ["Mili", "Lola", "Benja"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("Elemento seleccionado:", elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log("mostrando:", elemento);
+  };
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
+      {/* <List data={list} /> */}
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,20 +2,22 @@ import { MouseEvent, useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   // let index = 1;
   const [index, setIndex] = useState(1);
-  const handleClick = (i: number) => {
+  const handleClick = (i: number, elemento: string) => {
     //
     setIndex(i);
+    onSelect?.(elemento);
   };
   return (
     <ul className="list-group">
       {data.map((elemento, i) => (
         <li
-          onClick={() => handleClick(i)}
+          onClick={() => handleClick(i, elemento)}
           key={elemento}
           className={`list-group-item ${index == i ? "active" : ""}`}
         >
