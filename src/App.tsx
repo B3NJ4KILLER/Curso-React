a8a2f9c74c6174be7f902abb522838e03a107cdb
B3NJ4KILLER <benjacevedo12311@gmail.com>
2026-01-13 21:33:21 -0300

    Truthy y Falsy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+// * Truthy
+// * Falsy -->  0, "", null, undefined, NaN, false
 
 function App() {
-  const list = ["Mili", "Lola", "Benja"];
+  const list: string[] = [];
 
   const handleSelect = (elemento: string) => {
     console.log("Elemento seleccionado:", elemento);
   };
-  const handleSelect2 = (elemento: string) => {
-    console.log("mostrando:", elemento);
-  };
+  // const handleSelect2 = (elemento: string) => {
+  //   console.log("mostrando:", elemento);
+  // };
   return (
     <Card>
+      {/* {"" && "string vacio"}
+      {undefined && "indefinido"}
+      {null && "nulo"}
+      {false && "falso"}
+      {0 && "numero cero"} */}
+      {list.length !== 0 && "mi lista"}
       <CardBody title="Hola Mundo" text="Este es el texto" />
       <List data={list} onSelect={handleSelect} />
-      <List data={list} onSelect={handleSelect2} />
+      {/* <List data={list} onSelect={handleSelect2} /> */}
       {/* <List data={list} /> */}
     </Card>
   );
