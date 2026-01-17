dbec08b43850b2ca953d4000f97d693afbfa9ae8
B3NJ4KILLER <benjacevedo12311@gmail.com>
2026-01-16 21:45:36 -0300

    Gestor de Minions

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,51 @@ import { useState } from "react";
 // * Falsy -->  0, "", null, undefined, NaN, false
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const handleClick = () => setIsLoading(!isLoading);
+  const [data, setData] = useState(["benja", "lola", "mili"]);
 
-  const list: string[] = ["benja", "lola", "mili"];
+  // const [isLoading, setIsLoading] = useState(false);
+  // const handleClick = () => setIsLoading(!isLoading);
 
-  const handleSelect = (elemento: string) => {
-    console.log("Elemento seleccionado:", elemento);
-  };
-  // const handleSelect2 = (elemento: string) => {
-  //   console.log("mostrando:", elemento);
+  // const list: string[] = ["benja", "lola", "mili"];
+
+  // const handleSelect = (elemento: string) => {
+  //   console.log("Elemento seleccionado:", elemento);
   // };
+  // // const handleSelect2 = (elemento: string) => {
+  // //   console.log("mostrando:", elemento);
+  // // };
 
-  const contenido = list.length !== 0 && (
-    <List data={list} onSelect={handleSelect} />
-  );
+  // const contenido = list.length !== 0 && (
+  //   <List data={list} onSelect={handleSelect} />
+  // );
+  // return (
+  //   <Card>
+  //     {/* {"" && "string vacio"}
+  //     {undefined && "indefinido"}
+  //     {null && "nulo"}
+  //     {false && "falso"}
+  //     {0 && "numero cero"} */}
+  //     {/* {list.length !== 0 && "mi lista"} */}
+  //     <CardBody title="Hola Mundo" text="Este es el texto" />
+  //     {contenido}
+  //     <Button isLoading={isLoading} onClick={handleClick}>
+  //       Hola Mundo
+  //     </Button>
+
+  //     {/* <List data={list} onSelect={handleSelect} /> */}
+  //     {/* <List data={list} onSelect={handleSelect2} /> */}
+  //     {/* <List data={list} /> */}
+  //   </Card>
+  // );
+
+  const addMinion = () => setData([...data, "Minion"]);
+  const delMinion = () => setData(data.slice(0, -1));
+
   return (
     <Card>
-      {/* {"" && "string vacio"}
-      {undefined && "indefinido"}
-      {null && "nulo"}
-      {false && "falso"}
-      {0 && "numero cero"} */}
-      {/* {list.length !== 0 && "mi lista"} */}
-      <CardBody title="Hola Mundo" text="Este es el texto" />
-      {contenido}
-      <Button isLoading={isLoading} onClick={handleClick}>
-        Hola Mundo
-      </Button>
-
-      {/* <List data={list} onSelect={handleSelect} /> */}
-      {/* <List data={list} onSelect={handleSelect2} /> */}
-      {/* <List data={list} /> */}
+      <Button onClick={addMinion}>Agregar</Button>
+      <Button onClick={delMinion}>Eliminar</Button>
+      <List data={data} />
     </Card>
   );
 }
